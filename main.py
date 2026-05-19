842d9f651b88ee151e00aeb532e18cb45ea198df
Thiago Saback <thiisaback@gmail.com>
2026-05-18 21:02:59 -0300

    refactor: refatora estrutura do projeto

--- main.py
+++
@@ -1,5 +0,0 @@
-from src.ingestao.ingestao import processar_ingestao
-
-if __name__ == '__main__':
-    # Inicia o processo de ingestão de dados do servidor FTP do DataSUS para o Amazon S3
-    processar_ingestao()
